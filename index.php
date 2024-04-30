462318aa98d54098f879930fdd47598029778d28
Samuel Burla <samuelburla4@gmail.com>
2024-04-30 21:10:30 +0200

    visuel

--- index.php
+++ index.php
@@ -20,29 +20,59 @@ require_once __DIR__ . "/templates/header.php";
         , optio officia error</p>
     <img src="/assets/images/flamandRose.jpg" alt="Flamand Rose">
 </section>
+
+
 <section class="section_gallery">
     <h2>Les differents habitats</h2>
-    <div class="section_gallery_images">
-        <div class="section_gallery_images_img">
-
-            <img src="/assets/images/andreas-rasmussen-NNe6epzHGm8-unsplash (1).jpg" alt="s">
+    <div class="section_gallery_content">
+        <div class="section_gallery_images">
+            <div class="section_gallery_images_img">
+                <img src="/assets/images/andreas-rasmussen-NNe6epzHGm8-unsplash (1).jpg" alt="s">
+            </div>
+            <div class="section_gallery_images_img">
+                <img src="/assets/images/andreas-rasmussen-NNe6epzHGm8-unsplash (1).jpg" alt="s">
+            </div>
+            <div class="section_gallery_images_img">
+                <img src="/assets/images/andreas-rasmussen-NNe6epzHGm8-unsplash (1).jpg" alt="s">
+            </div>
+            <div class="section_gallery_images_img">
+                <img src="/assets/images/andreas-rasmussen-NNe6epzHGm8-unsplash (1).jpg" alt="s">
+            </div>
         </div>
-        <div class="section_gallery_images_img">
-
-            <img src="/assets/images/andreas-rasmussen-NNe6epzHGm8-unsplash (1).jpg" alt="s">
-        </div>
-        <div class="section_gallery_images_img">
-
-            <img src="/assets/images/andreas-rasmussen-NNe6epzHGm8-unsplash (1).jpg" alt="s">
-        </div>
-        <div class="section_gallery_images_img">
-
-            <img src="/assets/images/andreas-rasmussen-NNe6epzHGm8-unsplash (1).jpg" alt="s">
-        </div>
+        <a class="button" href="/habitats.php">Voir les habitats</a>
     </div>
 </section>
 
+<section class="section_gallery">
+    <h2>Les differents services</h2>
+    <div class="section_gallery_content">
+        <div class="section_gallery_images">
+            <div class="section_gallery_images_img">
+                <img src="/assets/images/andreas-rasmussen-NNe6epzHGm8-unsplash (1).jpg" alt="s">
+            </div>
+            <div class="section_gallery_images_img">
+                <img src="/assets/images/andreas-rasmussen-NNe6epzHGm8-unsplash (1).jpg" alt="s">
+            </div>
+            <div class="section_gallery_images_img">
+                <img src="/assets/images/andreas-rasmussen-NNe6epzHGm8-unsplash (1).jpg" alt="s">
+            </div>
+            <div class="section_gallery_images_img">
+                <img src="/assets/images/andreas-rasmussen-NNe6epzHGm8-unsplash (1).jpg" alt="s">
+            </div>
+        </div>
+        <a class="button" href="/habitats.php">Voir les services</a>
+    </div>
+</section>
+
+<div class="comment"></div>
+
+<!--
+<section class="section_image">
+<img src="/assets/images/arleen-wiese-2vbhN2Yjb3A-unsplash (1).jpg" alt="d">
+</section>
+-->
+
 
 <?php
-//require_once __DIR__ . "/templates/footer.php";
+// require_once __DIR__ . "/templates/footer.php";
 ?>
