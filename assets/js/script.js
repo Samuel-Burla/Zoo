7df041b8c77689661bd8a7559daf27dfab2dafd0
Samuel Burla <samuelburla4@gmail.com>
2024-05-15 00:05:09 +0200

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,108 @@
+const menuList = document.getElementById("menu");
+const IconButton = document.getElementsByClassName("show_menu_button")[0];
 
+function showMenu() {
+  menuList.classList.toggle("show_menu");
+}
+IconButton.addEventListener("click", showMenu);
+const menuList = document.getElementById("menu");
+const IconButton = document.getElementsByClassName("show_menu_button")[0];
+
+function showMenu() {
+  menuList.classList.toggle("show_menu");
+}
+IconButton.addEventListener("click", showMenu);
+const menuList = document.getElementById("menu");
+const IconButton = document.getElementsByClassName("show_menu_button")[0];
+
+function showMenu() {
+  menuList.classList.toggle("show_menu");
+}
+IconButton.addEventListener("click", showMenu);
+const menuList = document.getElementById("menu");
+const IconButton = document.getElementsByClassName("show_menu_button")[0];
+
+function showMenu() {
+  menuList.classList.toggle("show_menu");
+}
+IconButton.addEventListener("click", showMenu);
+const menuList = document.getElementById("menu");
+const IconButton = document.getElementsByClassName("show_menu_button")[0];
+
+function showMenu() {
+  menuList.classList.toggle("show_menu");
+}
+IconButton.addEventListener("click", showMenu);
+const menuList = document.getElementById("menu");
+const IconButton = document.getElementsByClassName("show_menu_button")[0];
+
+function showMenu() {
+  menuList.classList.toggle("show_menu");
+}
+IconButton.addEventListener("click", showMenu);
+const menuList = document.getElementById("menu");
+const IconButton = document.getElementsByClassName("show_menu_button")[0];
+
+function showMenu() {
+  menuList.classList.toggle("show_menu");
+}
+IconButton.addEventListener("click", showMenu);
+const menuList = document.getElementById("menu");
+const IconButton = document.getElementsByClassName("show_menu_button")[0];
+
+function showMenu() {
+  menuList.classList.toggle("show_menu");
+}
+IconButton.addEventListener("click", showMenu);
+const menuList = document.getElementById("menu");
+const IconButton = document.getElementsByClassName("show_menu_button")[0];
+
+function showMenu() {
+  menuList.classList.toggle("show_menu");
+}
+IconButton.addEventListener("click", showMenu);
+const menuList = document.getElementById("menu");
+const IconButton = document.getElementsByClassName("show_menu_button")[0];
+
+function showMenu() {
+  menuList.classList.toggle("show_menu");
+}
+IconButton.addEventListener("click", showMenu);
+const menuList = document.getElementById("menu");
+const IconButton = document.getElementsByClassName("show_menu_button")[0];
+
+function showMenu() {
+  menuList.classList.toggle("show_menu");
+}
+IconButton.addEventListener("click", showMenu);
+const menuList = document.getElementById("menu");
+const IconButton = document.getElementsByClassName("show_menu_button")[0];
+
+function showMenu() {
+  menuList.classList.toggle("show_menu");
+}
+IconButton.addEventListener("click", showMenu);
+const menuList = document.getElementById("menu");
+const IconButton = document.getElementsByClassName("show_menu_button")[0];
+
+function showMenu() {
+  menuList.classList.toggle("show_menu");
+}
+IconButton.addEventListener("click", showMenu);
+const menuList = document.getElementById("menu");
+const IconButton = document.getElementsByClassName("show_menu_button")[0];
+
+function showMenu() {
+  menuList.classList.toggle("show_menu");
+}
+IconButton.addEventListener("click", showMenu);
+const menuList = document.getElementById("menu");
+const IconButton = document.getElementsByClassName("show_menu_button")[0];
+
+function showMenu() {
+  menuList.classList.toggle("show_menu");
+}
+IconButton.addEventListener("click", showMenu);
 const menuList = document.getElementById("menu");
 const IconButton = document.getElementsByClassName("show_menu_button")[0];
 
